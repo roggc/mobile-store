743b2e78eaff4010951e7475acc27740b4c70868
roggc <roggc9@gmail.com>
2023-06-09 08:42:22 +0200

    rcs 7.2.1

--- src/slices.js
+++ src/slices.js
@@ -1,7 +1,7 @@
 import getHookAndProviderFromSlices from 'react-context-slices'
 
 export const { useSlice, Provider } = getHookAndProviderFromSlices({
-    count: { initialState: 0 },
-    products: { initialState: [] },
-    time: { initialState: 0 },
+    count: { initialArg: 0 },
+    products: { initialArg: [] },
+    time: { initialArg: 0 },
 })
